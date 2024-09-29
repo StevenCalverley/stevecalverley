5545da5d05ea1c1270cf9c471f41061bf6dd0d8c
Steven Calverley <steven@stevencalverley.com>
2024-09-29 12:16:39 +1000

    Upgrade to NextJS 13

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,18 +21,20 @@ const Home: NextPage = () => {
               Steve Calverley.
             </h1>
             <h2 className="mt-2 text-gray-700 dark:text-gray-200 mb-4">
-              Lead Frontend Developer at{' '}
+              Technical Product Manager at{' '}
               <span className="font-semibold">
-                <Link href="https://www.heffron.com.au" passHref>
-                  <a target="_blank" rel="noopener noreferrer">
-                    Heffron SMSF Consulting
-                  </a>
+                <Link
+                  href="https://chl.global/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  CHL Group
                 </Link>
               </span>
             </h2>
             <p className="text-gray-600 dark:text-gray-400 mb-16">
-              Developing software that simplifies SMSF administration and
-              enhance client experience.
+              Developing software that simplifies the peer to peer market
+              withing the camping industry.
             </p>
           </div>
           <div className="w-[80px] sm:w-[176px] relative">
