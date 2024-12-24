4da78eef5c16e9cd0cbacb7aba7b7ff7cb44f1e8
Steven Calverley <steven@stevencalverley.com>
2024-12-24 13:40:53 +1100

    Update home page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,18 +15,28 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto max-w-3xl py-8">
-        <article className="flex">
-          <div className="flex flex-col pr-8">
-            <h1 className="font-bold text-3xl md:text-5xl tracking-tight text-black dark:text-white">
-              Steve Calverley.
+        <article className="flex flex-col px-4">
+          <div className="flex justify-between items-end">
+            <h1 className="font-bold text-3xl sm:text-5xl tracking-tight text-black dark:text-white">
+              Steve Calverley
             </h1>
+            <div className="w-[80px] sm:w-[176px] relative">
+              <Image
+                src={SteveCalverley}
+                alt="Steve Calverley"
+                className="rounded-full filter grayscale"
+              />
+            </div>
           </div>
-          <div className="w-[80px] sm:w-[176px] relative">
-            <Image
-              src={SteveCalverley}
-              alt="Steve Calverley"
-              className="rounded-full filter grayscale"
-            />
+          <div>
+            <p className="mt-8 text-gray-500 dark:text-gray-400 max-w-xl">
+              Hard-working web developer with a flair for creating elegant
+              web-based solutions on time and within budget. I build solutions
+              that elevate experiences and empower businesses. Developed a suite
+              of web applications supporting business objectives to enhance
+              customer experience. Passionate about software architecture and
+              user experience.
+            </p>
           </div>
         </article>
       </main>
